898518641cff2fd4f09f96fd5fb6875cdd6b20ab
abfd <abfd@valtecnic.com>
2018-06-05 17:02:07 +0200

    primera subida del paquete Caja Rural

---
+++ src/CajaRural/envioHistoricas.java
@@ -0,0 +1,165 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CajaRural;
+
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import xmlCajaRural.ValoracionHistoricas;
+
+/**
+ *
+ * @author Ana Belen de Frutos
+ * * Proceso que lee de un excel los expedientes cuyo estado es ENVIAR. Si el estado es NO ENVIAR se descarta su envio.
+ */
+public class envioHistoricas 
+{
+    private java.sql.Connection conexion = null; 
+    private Logger logger = Logger.getLogger(envioHistoricas.class);
+    
+    public static String sUrlExcel = null;
+    public static Utilidades.Excel oExcel = null;            
+    public static org.apache.poi.hssf.usermodel.HSSFCell celda = null;
+    public static String HojaActual = null;
+    
+    public static void main(String[] args) 
+    {
+         //FICHERO LOG4J              
+        envioHistoricas nwEnvio = new envioHistoricas();
+        nwEnvio = null;
+        System.gc();
+    }
+    
+    
+    public envioHistoricas()
+    {//EN EL EXCEL COLUMNA 1 = EXPEDIENTE COLUMNA 2 = ESTADO ENVIAR/NO ENVIAR
+        
+         Objetos.v2.Solicitudes oSolicitudes = new Objetos.v2.Solicitudes();
+         String numexp = "";       
+        
+         try
+         {
+            conexion = Utilidades.Conexion.getConnection("jdbc:oracle:thin:valtecnic2/valtecnic2@oraserver02:1521:rvtnprod");            
+            conexion.setAutoCommit(false);                                
+              
+            
+            sUrlExcel = "/data/informes/CajaRural/historicas/historicas.xls";
+            oExcel = new Utilidades.Excel(sUrlExcel);              
+            celda = null;
+            HojaActual = "Hoja1";
+            int totalFilas = 22;
+            for (int fila = 0; fila < totalFilas; fila ++)
+            {                                                
+                celda = oExcel.getCeldaFilaHoja(HojaActual,fila,0);                                 
+                numexp = Utilidades.Excel.getStringCellValue(celda);                  
+                xmlCajaRural.XMLTasacionRural nwValoracion = new xmlCajaRural.XMLTasacionRural(numexp);         
+                //xmlCajaRural.ValoracionHistoricas nwValoracion = new xmlCajaRural.ValoracionHistoricas(numexp,1);                  
+                PropertyConfigurator.configure("/data/informes/CajaRural/historicas/" + "Log4j.properties");   
+                if (nwValoracion.estadoOK) 
+                {                    
+                    //if (nwValoracion.masDe1Finca) logger.info("Tasacion con mas de 1 finca registral generada: "+numexp);
+                    logger.info("Tasación generada: "+numexp);
+                }
+                else
+                {
+                    logger.error("Error: "+numexp+". Descripción: "+nwValoracion.descError);
+                    try
+                    {
+                        Utilidades.Correo.sendMail("valtecnic.com", "informatica@valtecnic.com", "abfd@valtecnic.com", "", "", numexp, nwValoracion.descError, true);
+                    }
+                    catch(Exception e)
+                    {
+                        logger.error("Excepción envio mail: "+e.toString());
+                    }
+                }
+                nwValoracion = null;
+                try
+                {//ESPERAMOS 4 SEGUNDOS ANTES DE PROCESAR EL SIGUEINTE SOLICITUD                                   
+                    Thread.currentThread().sleep(2000);
+                }
+                catch (InterruptedException ie)
+                {                                                                                                
+                     logger.error(" Imposible realizar espera");
+                }
+            }//for excel
+            conexion.close();
+         }//try
+         catch (Exception e)
+         {
+             logger.error("Excepcion: "+e.toString());
+         }
+         finally
+         {
+             try
+             {
+                if (conexion != null && !conexion.isClosed())
+                {                
+                     conexion.rollback();
+                     conexion.close();
+                }
+             }
+             catch (Exception e)
+             {
+                 
+             }                 
+         }
+    }//envioDOTA
+    
+    public void envioDOTA()
+    {//EN EL EXCEL COLUMNA 1 = EXPEDIENTE COLUMNA 2 = ESTADO ENVIAR/NO ENVIAR
+        
+        
+         String numexp = "";
+         String estado = "";
+         int totales = 0;
+         int aEnviar = 0;
+         int noEnviar = 0;
+         int lote = 100;
+         String sConsulta = null;
+         String sUpdate = null;
+         java.sql.ResultSet rs = null;
+         Objetos.v2.Operclientes oOperclientes = new Objetos.v2.Operclientes();
+        
+         try
+         {            
+              
+            
+            sUrlExcel = "/data/informes/Servihabitat/enviosDOTA/enviosdota1.xls";
+            oExcel = new Utilidades.Excel(sUrlExcel);              
+            celda = null;
+            HojaActual = "RESUMEN";
+            int totalFilas = 1471;
+            for (int fila = 0; fila < totalFilas; fila ++)
+            {//
+                 totales ++;
+                 if (numexp != null)
+                 {
+                    numexp = oExcel.getCeldaFilaHoja(HojaActual, fila, 0).getStringCellValue();
+                    estado = oExcel.getCeldaFilaHoja(HojaActual, fila, 1).getStringCellValue();
+                    if (estado != null)
+                    {
+                         if (estado.trim().toUpperCase().equals("ENVIAR")) aEnviar ++;                        
+                         else if (estado.trim().toUpperCase().equals("NO ENVIAR")) noEnviar ++;
+                    }
+                    else logger.error("El estado del expediente : "+numexp +"es nulo");
+                 }
+                 else logger.error("El expediente de la fila : "+fila+1 +"es nulo");
+                 
+            }//for excel            
+         }//try
+         catch (Exception e)
+         {
+             logger.error("Excepcion: "+e.toString());
+         }
+         finally
+         {
+            
+             logger.info("Totales : "+totales);
+             logger.info("A enviar : "+aEnviar);             
+             logger.info("Noenviar : "+noEnviar);             
+         }
+    }//envioDOTA
+    
+}
